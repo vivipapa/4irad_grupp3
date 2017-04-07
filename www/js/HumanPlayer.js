5fbabf1355f98ec8d2f1034ff040fbdb07259b6c
tatianavdovina <tvdovina1983@gmail.com>
2017-04-07 15:15:10 +0200

    restoring correct main.js contents

--- www/js/HumanPlayer.js
+++ www/js/HumanPlayer.js
@@ -1,5 +1,6 @@
 class HumanPlayer extends Player {
-
+  constructor(player_name, player_number, player_type){
+  	super(player_name, player_number, player_type);
+  }	
   makeMove(column){}
-
 }
--- www/js/Player.js
+++ www/js/Player.js
@@ -1,4 +1,10 @@
 class Player {
   // some properties: name, score (numberOfMove) 
   // one possible method: makeMove
+   constructor(player_name, player_number, player_type){
+  	this.name = player_name;
+  	this.number = player_number;
+  	this.type = player_type;
+  }
+
 }
