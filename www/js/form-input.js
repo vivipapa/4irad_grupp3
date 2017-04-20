0291fb33ddb9d85b6b86c3f4c6dff96f2bb96eb6
Wondi <wonwud@yahoo.com>
2017-04-20 11:50:20 +0200

    Corrected the number of winning moves

--- www/js/form-input.js
+++ www/js/form-input.js
@@ -51,7 +51,7 @@ function readContactForm(){
 
   // Read values from the form input fields
   let player1 = new Player($('#player1_name').val(), $('input[name=player1_type]:checked').val(),'red',1, false);
-  let player2 = new Player($('#player2_name').val(), $('input[name=player2_type]:checked').val(),'yellow',1, false);
+  let player2 = new Player($('#player2_name').val(), $('input[name=player2_type]:checked').val(),'yellow',0, false);
   window.players = [player1,player2];
   
   // Check if the return of the checkPlayers is true and if so stay on the same page
